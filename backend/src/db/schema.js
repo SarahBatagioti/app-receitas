9a9f15913fcd1244d52a0561a49042c52437f5f0
SarahBatagioti <sarah.montuanibt@gmail.com>
2025-09-29 02:45:25 -0300

    feat: implementado endpoint de API de favoritos e atualizar esquema de banco de dados

--- backend/src/db/schema.js
+++ backend/src/db/schema.js
@@ -6,7 +6,7 @@ export const favoritesTable = pgTable('favorites', {
   recipeId: integer('recipe_id').notNull(),
   title: text('title').notNull(),
   image: text('image'),
-  cookieTime: text('cookie_time'),
+  cookTime: text('cook_time'),
   servings: text('servings'),
   createdAt: timestamp('created_at').defaultNow(),
 });
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,42 @@
 import express from 'express';
 import { ENV } from './config/env.js';
+import { db } from './config/db.js';
+import { favoritesTable } from './db/schema.js';
 
 const app = express();
 const PORT = ENV.PORT|| 3001;
 
+app.use(express.json());
+
 app.get('/api/health', (req, res) => {
   res.status(200).send({ success: true }); 
+});
+
+app.post('/api/favorites', async (req, res) => {
+  try {
+    const { userId, recipeId, title, image, cookTime, servings } = req.body;
+    if (!userId || !recipeId || !title) {
+      return res.status(400).send({ error: 'Arquivos ausentes' });
+    }
+
+    const newFavorite = await db
+      .insert(favoritesTable)
+      .values({
+        userId,
+        recipeId,
+        title,
+        image,
+        cookTime,
+        servings
+      })
+      .returning();
+
+    res.status(201).json(newFavorite[0]);
+    
+  } catch (error) {
+    console.error('Erro ao adicionar favorito:', error);
+    res.status(500).json({ error: 'Erro ao adicionar favorito' });
+  }
 });
 
 app.listen(PORT, () => {
