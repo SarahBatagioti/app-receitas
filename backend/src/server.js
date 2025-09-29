42bdee5ba51baf5d1e2c2e3b1a56bae4b68eece2
SarahBatagioti <sarah.montuanibt@gmail.com>
2025-09-29 03:02:18 -0300

    feat: adicionado CRUD para favoritos na API

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,16 @@ import express from 'express';
 import { ENV } from './config/env.js';
 import { db } from './config/db.js';
 import { favoritesTable } from './db/schema.js';
+import { eq, and } from 'drizzle-orm';
+import { parse } from 'dotenv';
 
 const app = express();
-const PORT = ENV.PORT|| 3001;
+const PORT = ENV.PORT || 3001;
 
 app.use(express.json());
 
 app.get('/api/health', (req, res) => {
-  res.status(200).send({ success: true }); 
+  res.status(200).send({ success: true });
 });
 
 app.post('/api/favorites', async (req, res) => {
@@ -32,10 +34,43 @@ app.post('/api/favorites', async (req, res) => {
       .returning();
 
     res.status(201).json(newFavorite[0]);
-    
+
   } catch (error) {
     console.error('Erro ao adicionar favorito:', error);
     res.status(500).json({ error: 'Erro ao adicionar favorito' });
+  }
+});
+
+app.get('/api/favorites/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const userFavorites = await db
+      .select()
+      .from(favoritesTable)
+      .where(eq(favoritesTable.userId, userId));
+
+    res.status(200).json(userFavorites);
+
+  } catch (error) {
+    console.error('Erro ao buscar favoritos:', error);
+    res.status(500).json({ error: 'Erro ao buscar favoritos' });
+  }
+});
+
+app.delete('/api/favorites/:userId/:recipeId', async (req, res) => {
+  try {
+    const { userId, recipeId } = req.params;
+
+    await db
+      .delete(favoritesTable)
+      .where(and(eq(favoritesTable.userId, userId), eq(favoritesTable.recipeId, parseInt(recipeId)))
+      );
+
+    res.status(200).json({ message: 'Favorito removido com sucesso' });
+  } catch (error) {
+    console.error('Erro ao remover favorito:', error);
+    res.status(500).json({ error: 'Erro ao remover favorito' });
   }
 });
 
